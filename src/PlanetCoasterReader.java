3499c15d0013a4f7d966951c18141e51b8f822e0
ScrappyCocco <mikelerogeratro@yahoo.it>
2017-12-08 19:54:24 +0100

    Better code style
    First steps of code improvement

--- src/PlanetCoasterReader.java
+++ src/PlanetCoasterReader.java
@@ -1,4 +1,5 @@
 //IMPORTS
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Comment;
 import org.w3c.dom.Document;
@@ -7,23 +8,24 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXParseException;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 //CLASS
+
 /**
  * This class read a single planet coaster xml file storing the data in two arrays: Keys and Values
  * The Keys array is made of Strings because the translations Keys are simple strings
- * The translated phrase is stored into a byte[] because i can store it in UTF-8
- * */
+ * The translated phrase is stored into a byte[] because i can store it in UTF-8 (or another encoding)
+ */
 class PlanetCoasterReader {
     //----------------------------------------------------------
-    private Window window_reference;
+    private Window window_reference; //reference to main window to use print_log()
     ArrayList<String> Keys; //The translation Keys
     ArrayList<byte[]> utf8_values; //The translation values
     private boolean isnewFile; //if is the new file i need to store comments
@@ -31,57 +33,60 @@ class PlanetCoasterReader {
 
     /**
      * The constructor method open the file and call a method to fill the arrays
+     *
      * @param file_top_open the path with the filename to open
-     * @param isfinalfile if is the final file i need to consider comments
-     * @param ref reference to main window to use print_log()
+     * @param isfinalfile   if is the final file i need to consider comments
+     * @param ref           reference to main window to use print_log()
      * @throws Exception throw an invalid UTF-8 charset exception (from scan_node()) or a generic Exception if the XML file is not valid
      */
-    PlanetCoasterReader(String file_top_open, boolean isfinalfile, Window ref) throws Exception{
+    PlanetCoasterReader(String file_top_open, boolean isfinalfile, Window ref) throws Exception {
         window_reference = ref;
         Element root; //The root element of the xml file
         Document document_file;
-        isnewFile=isfinalfile; //if is the new file i need to store comments
+        isnewFile = isfinalfile; //if is the new file i need to store comments
         //creating the array lists
-        Keys=new ArrayList<String>();
+        Keys = new ArrayList<String>();
         utf8_values = new ArrayList<byte[]>();
         //-------------------------------------------------------------------------
-        try{
+        try {
             //Opening the file
-            DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder domParser = dbf.newDocumentBuilder();
-            document_file=domParser.parse(new File(file_top_open));
-            root=document_file.getDocumentElement();
-            Node start_node=root.getFirstChild();
+            document_file = domParser.parse(new File(file_top_open));
+            root = document_file.getDocumentElement();
+            Node start_node = root.getFirstChild();
             //Choosing the First node
-            if(!start_node.getNodeName().equals("translation")){ //The first node is not "translation"
-                start_node=start_node.getNextSibling(); //Check the node after the first
-                if(!start_node.getNodeName().equals("translation")){ //The xml file is not correct
-                    throw new Exception("Bad PlanetCoaster-XML File! Check your XML file"); //can't find the start node, error
+            if (!start_node.getNodeName().equals("translation")) { //The first node is not "translation"
+                start_node = start_node.getNextSibling(); //Check the node after the first
+                if (!start_node.getNodeName().equals("translation")) { //The xml file is not correct
+                    //can't find the start node, error
+                    throw new Exception("Bad PlanetCoaster-XML File! Check your XML file");
                 }
             }
             scan_node(start_node); //starting from the root
-        }catch(SAXParseException e){ //error reading the xml file
-            throw new Exception("Error(SAXParseException)-->"+e.getMessage());
-        }catch(FileNotFoundException e){ //error opening the file
-            throw new Exception("Error(FileNotFoundException)-->"+e.getMessage());
-        }catch(Exception e){ //generic error
+        } catch (SAXParseException e) { //error reading the xml file
+            throw new Exception("Error(SAXParseException)-->" + e.getMessage());
+        } catch (FileNotFoundException e) { //error opening the file
+            throw new Exception("Error(FileNotFoundException)-->" + e.getMessage());
+        } catch (Exception e) { //generic error
             e.printStackTrace();
-            throw new Exception("Generic Error-->"+e.getMessage());
+            throw new Exception("Generic Error-->" + e.getMessage());
         }
     }//Constructor
 
     /**
      * This method scan all the nodes under the root
-     * Scanning all the nodes this method fill the two arrays with all the values
+     * While scanning all the nodes, this method fill the two arrays with all the values
+     *
      * @param node the root node, where i start to scan
      * @throws Exception if the string format is not valid
      */
-    private void scan_node(Node node) throws Exception{
+    private void scan_node(Node node) throws Exception {
         window_reference.print_log("---------------------------");
         window_reference.print_log("Inside the XML File");
-        NodeList child_nodes_list=node.getChildNodes(); //Entering root childs
+        NodeList child_nodes_list = node.getChildNodes(); //Entering root childs
         int childs_count = child_nodes_list.getLength(); //Counting childrens
-        window_reference.print_log("Childs:"+childs_count);
+        window_reference.print_log("Childs:" + childs_count);
         try {
             for (int i = 0; i < childs_count; i++) { //For every child
                 node = child_nodes_list.item(i); //I get the node
@@ -101,9 +106,9 @@ class PlanetCoasterReader {
                     }//is_new_file
                 }//else_is_a_comment
             }//for_en
-        }catch (Exception e){
+        } catch (Exception e) {
             window_reference.print_log("An error occurred during the scan of the xml file!");
-            throw new Exception("Scan node error-->"+e.getMessage());
+            throw new Exception("Scan node error-->" + e.getMessage());
         }
         window_reference.print_log("Arrays Created!");
         window_reference.print_log("---------------------------");
--- src/PlanetCoasterWriter.java
+++ src/PlanetCoasterWriter.java
@@ -1,8 +1,10 @@
 //IMPORTS
+
 import org.w3c.dom.Attr;
 import org.w3c.dom.Comment;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
+
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,9 +14,11 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+
 import org.w3c.dom.Element;
 import org.w3c.dom.ls.DOMImplementationLS;
 import org.w3c.dom.ls.LSSerializer;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -26,94 +30,119 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 //CLASS
+
 /**
  * This class call the Reader for the 2 files and merge them into a single array, then write it into the final file
- * */
+ */
 class PlanetCoasterWriter {
     //----------------------------------------------------------
     //Copy array to fast access
-    private ArrayList<String> oldKeys, newKeys;
-    private ArrayList<byte[]> oldUTFTrans, newUTFTrans;
+    private ArrayList<String> oldKeys, newKeys; //old file arrays
+    private ArrayList<byte[]> oldUTFTrans, newUTFTrans; //new file arrays
     boolean has_finished = false; //for the thread, to check the end of the execution
-    private Window window_reference;
+    private Window window_reference; //reference to main window to use print_log()
     //----------------------------------------------------------
 
     /**
      * The constructor create the PlanetCoasterReader for the two files and the merge them
+     *
      * @param oldPath the old xml file path
      * @param newPath the new xml file path
-     * @param ref reference to main window to use print_log()
+     * @param ref     reference to main window to use print_log()
      * @throws Exception exceptions for generic errors
      */
-    PlanetCoasterWriter(String oldPath,String newPath, Window ref) throws Exception{
+    PlanetCoasterWriter(String oldPath, String newPath, Window ref) throws Exception {
         window_reference = ref;
         Document xml_document;
         String output_file_name = "Final.xml";
-
+        //----------------------------------------------------------
+        //reading the two files
         PlanetCoasterReader oldFile, newFile;
         try {
             window_reference.print_log("First File:");//first file (old)
             oldFile = new PlanetCoasterReader(oldPath, false, ref);
             window_reference.print_log("Second File:"); //second file (new)
             newFile = new PlanetCoasterReader(newPath, true, ref);
-        }catch(Exception err){
+        } catch (Exception err) {
             throw new Exception(err.toString());
         }
+        //----------------------------------------------------------
         //Setting array references to keys and values
-        oldKeys=oldFile.Keys;
-        oldUTFTrans=oldFile.utf8_values;
+        oldKeys = oldFile.Keys;
+        oldUTFTrans = oldFile.utf8_values;
 
-        newKeys=newFile.Keys;
-        newUTFTrans=newFile.utf8_values;
-
-        window_reference.print_log("Char utf8_values:"); //Print test - 1 (check if a common key exist)
-        if(newKeys.indexOf("TrackElementDesc_TK_SP_Immelmann_180Left")==-1){
+        newKeys = newFile.Keys;
+        newUTFTrans = newFile.utf8_values;
+        //----------------------------------------------------------
+        //Print test - 1 (check if a common key exist)
+        window_reference.print_log("Char utf8_values:");
+        if (newKeys.indexOf("TrackElementDesc_TK_SP_Immelmann_180Left") == -1) {
             throw new Exception("PLANET COASTER KEY NOT FOUND - does it exist?");
-        }else {
+        } else {
             window_reference.print_log(new String(newUTFTrans.get(newKeys.indexOf("TrackElementDesc_TK_SP_Immelmann_180Left")), "UTF-8"));
         }
         //----------------------------------------------------------
+        //merging the two arrays
         window_reference.print_log("Starting array merge");
         try {
-            merge_arrays(); //merging the two arrays
-        }catch (Exception err){
+            merge_arrays();
+        } catch (Exception err) {
             err.printStackTrace();
             throw new Exception(err.toString());
         }
         window_reference.print_log("End of array merge");
         //----------------------------------------------------------
-        window_reference.print_log("Copy utf8_values:"); //Print test - 2
-        if(newKeys.indexOf("BuildingPartCategory_Building_Signs")==-1){ //(check if a common key exist)
+        //Print test - 2
+        window_reference.print_log("Copy utf8_values:");
+        if (newKeys.indexOf("BuildingPartCategory_Building_Signs") == -1) {
             throw new Exception("PLANET COASTER KEY NOT FOUND - does it exist?");
-        }else {
+        } else {
             window_reference.print_log(new String(newUTFTrans.get(newKeys.indexOf("BuildingPartCategory_Building_Signs")), "UTF-8"));
         }
         window_reference.print_log("---------------------------");
         //----------------------------------------------------------
         //start creating the new xml
-        DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
-        try{
+        xml_document = generate_xml_output();
+        //write xml to file
+        write_xml_file(xml_document, output_file_name);
+        //----------------------------------------------------------
+        window_reference.print_log("Execution Ended!");
+        has_finished = true;
+    }//Constructor
+
+
+    /**
+     * This function generate the xml Document from the newKeys/newUTFTrans arrays
+     *
+     * @return The generated XML document
+     * @throws Exception if an error occur, throws an exception
+     */
+    private Document generate_xml_output() throws Exception {
+        Document xml_document;
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
             DocumentBuilder builder = dbf.newDocumentBuilder();
-            xml_document =builder.newDocument();
-        }catch(ParserConfigurationException e){
+            xml_document = builder.newDocument();
+        } catch (ParserConfigurationException e) {
             e.printStackTrace();
             throw new Exception("Error (ParserConfigurationException) --> " + e.getMessage());
         }
         Element root_element = xml_document.createElement("localisation"); //creating root node
-        root_element.setAttribute("xmlns","http://www.planetcoaster.com/CommunityTranslation");
+        root_element.setAttribute("xmlns", "http://www.planetcoaster.com/CommunityTranslation");
         xml_document.appendChild(root_element); //Setting root child element
-        Element translation= xml_document.createElement("translation");
+        Element translation = xml_document.createElement("translation");
         //----------------------------------------------------------
         //create the xml with elements, comments, etc
         Element entry;
         Comment comment;
-        int comments_counter=0;
-        for(int i=0;i<newKeys.size();i++){ //for every key
-            if(newKeys.get(i).equals("Comment")){ //is a comment?
+        int comments_counter = 0;
+        for (int i = 0; i < newKeys.size(); i++) { //for every key
+            if (newKeys.get(i).equals("Comment")) { //is a comment?
                 comments_counter++;
                 comment = xml_document.createComment(new String(newUTFTrans.get(i), "UTF-8"));
                 translation.appendChild(comment);
-            }else { //is a normal entry
+            } else { //is a normal entry
                 entry = xml_document.createElement("entry"); //creating the node (tag)
                 Attr key = xml_document.createAttribute("key"); //creating the attr
                 key.setValue(newKeys.get(i));
@@ -124,36 +153,45 @@ class PlanetCoasterWriter {
                 translation.appendChild(entry);
             }
         }
-        window_reference.print_log("Comments:"+comments_counter);
+        window_reference.print_log("Comments:" + comments_counter);
         root_element.appendChild(translation);
         window_reference.print_log("---------------------------");
-        //----------------------------------------------------------
-        //write the xml to a file
-        DOMImplementation impl= xml_document.getImplementation();
-        DOMImplementationLS implLS=(DOMImplementationLS)impl.getFeature("LS","3.0");
-        LSSerializer ser=implLS.createLSSerializer();
-        String output=ser.writeToString(xml_document);
-        output=output.replace("UTF-16","utf-8"); //UTF-8
-        output=prettyFormat(output,2); //format the xml file
-        window_reference.print_log("Saving the file..." );
-        try{
+        return xml_document;
+    }
+
+
+    /**
+     * This function write the XML document to an XML file
+     *
+     * @param xml_document     the xml document to write
+     * @param output_file_name the output file name
+     * @throws Exception if an error occur during the creation of the file
+     */
+    private void write_xml_file(Document xml_document, String output_file_name) throws Exception {
+        DOMImplementation impl = xml_document.getImplementation();
+        DOMImplementationLS implLS = (DOMImplementationLS) impl.getFeature("LS", "3.0");
+        LSSerializer ser = implLS.createLSSerializer();
+        String output = ser.writeToString(xml_document);
+        output = output.replace("UTF-16", "utf-8"); //UTF-8
+        output = prettyFormat(output, 2); //format the xml file
+        window_reference.print_log("Saving the file...");
+        try {
             //write the file using utf-8
             Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(output_file_name), "UTF-8"));
             out.write(output);
             out.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             throw new Exception("Error (IOException) --> " + e.getMessage());
         }
-        window_reference.print_log("File Saved!\nExecution Ended!" );
-        has_finished = true;
-    }//Constructor
+        window_reference.print_log("File Saved!");
+    }
 
-    //----------------------------------------------------------
 
     /**
-     * This function format the xml file
-     * @param input the input xml to format
+     * This function format the xml file (NOT MINE)
+     *
+     * @param input  the input xml to format
      * @param indent the indentations for formatting the code (always 2 in this program)
      * @return the new xml code formatted
      */
@@ -173,33 +211,32 @@ class PlanetCoasterWriter {
         }
     }
 
-    //----------------------------------------------------------
 
     /**
      * This function merge the first array with the second,
      * leaving the translated sentences and adding the sentences from the new file
      */
-    private void merge_arrays() throws Exception{
+    private void merge_arrays() throws Exception {
         window_reference.print_log("---------------------------");
-        ArrayList<String> remKeys=new ArrayList<String>();
-        ArrayList<String> removed_values=new ArrayList<String>();
-        int start_size=oldKeys.size();
-        int values_found=0;
-        int values_removed=0;
-        while(oldKeys.size()>0){ //while there are old keys
-            int pos=newKeys.indexOf(oldKeys.get(0));
-            if(pos!=-1){ //if the old key exist in new file
+        ArrayList<String> remKeys = new ArrayList<String>();
+        ArrayList<String> removed_values = new ArrayList<String>();
+        int start_size = oldKeys.size();
+        int values_found = 0;
+        int values_removed = 0;
+        while (oldKeys.size() > 0) { //while there are old keys
+            int pos = newKeys.indexOf(oldKeys.get(0));
+            if (pos != -1) { //if the old key exist in new file
                 //write it
                 newUTFTrans.set(pos, oldUTFTrans.get(0));
                 oldKeys.remove(0);
                 oldUTFTrans.remove(0);
                 values_found++;
-            }else{
+            } else {
                 //the old key doesn't exist
                 values_removed++;
                 try { //saving the key as removed
                     removed_values.add(new String(oldUTFTrans.get(0), "UTF-8"));
-                }catch (java.io.UnsupportedEncodingException e){
+                } catch (java.io.UnsupportedEncodingException e) {
                     window_reference.print_log("FATAL ERROR - UnsupportedEncodingException");
                     throw new Exception("Error (UnsupportedEncodingException) --> " + e.getMessage());
                 }
@@ -209,11 +246,11 @@ class PlanetCoasterWriter {
                 oldKeys.remove(0);
             }
         }//while
-        window_reference.print_log("Start Size:"+start_size+" Found:"+values_found+" Removed:"+values_removed);
-        window_reference.print_log("Final Size OldArray:"+oldKeys.size());
+        window_reference.print_log("Start Size:" + start_size + " Found:" + values_found + " Removed:" + values_removed);
+        window_reference.print_log("Final Size OldArray:" + oldKeys.size());
         window_reference.print_log("Creating string loss file...");
-        try{ //print all the lost strings
-            if(values_removed>0) {
+        try { //print all the lost strings
+            if (values_removed > 0) {
                 PrintWriter writer = new PrintWriter("StringLoss.txt", "UTF-8");
                 if ((removed_values.size() != values_removed) || (remKeys.size() != values_removed)) {
                     window_reference.print_log("\n(Something's strange)\n");
@@ -222,11 +259,11 @@ class PlanetCoasterWriter {
                     writer.println("Key: \"" + remKeys.get(i) + "\" - Value: \"" + removed_values.get(i) + "\"");
                 }
                 writer.close();
-            }else{
+            } else {
                 window_reference.print_log("\nSkipped creation of StringLoss.txt, no string removed...");
             }
         } catch (Exception e) {
-            window_reference.print_log("\nError creating the file:"+e.toString());
+            window_reference.print_log("\nError creating the file:" + e.toString());
             e.printStackTrace();
             throw new Exception("Error --> " + e.getMessage());
         }
@@ -234,5 +271,4 @@ class PlanetCoasterWriter {
         window_reference.print_log("---------------------------");
     }
     //----------------------------------------------------------
-
-}
+}//end_class
